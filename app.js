0740d5d925dcfb705fb952fca40e29f5b555a57b
rajonhossain <rajonhossaindhaka@gmail.com>
2022-06-24 18:50:42 +0600

    t
    ss

--- app.js
+++ app.js
@@ -15,16 +15,16 @@ const flash = require('connect-flash');
 // var app = express();
 
 var app = express();
- 
+
 app.use(flash());
 
 
 
 app.use(session({
-	name: 'app.sid',
-	secret: "1234567890QWERTY",
-	resave: true,
-	saveUninitialized: true
+  name: 'app.sid',
+  secret: "1234567890QWERTY",
+  resave: true,
+  saveUninitialized: true
 }));
 
 
@@ -45,12 +45,12 @@ app.use('/admin', indexRouter);
 app.use('/desh_delivery', deliverman);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
